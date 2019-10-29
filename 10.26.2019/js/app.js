c974f2962be5949fe6666105ba5f17587f2427ef
Vagifidrak <54859413+Vagifidrak@users.noreply.github.com>
2019-10-29 05:28:17 -0800

    Add files via upload

--- 10.26.2019/js/app.js
+++ 10.26.2019/js/app.js
@@ -16,8 +16,31 @@ next.onclick = function () {
     vaqif--;
 
     if (vaqif == -1) {
-        vaqif = sliderUl.querySelectorAll("li").length-1
+        vaqif = sliderUl.querySelectorAll("li").length - 1
     }
     sliderUl.style.left = -800 * vaqif + "px"
 
+}
+
+
+var nextbloom = document.getElementById("nextbloom")
+var prewbloom = document.getElementById("prewbloom")
+var sliderbloom = document.querySelector(".sliderbloom ul")
+var start = 0;
+
+prewbloom.onlick = function () {
+    start++;
+
+    if (start == sliderbloom.querySelectorAll("li").lenght) {
+        start = 0
+    }
+    sliderbloom.style.left = -800 * start + "px"
+}
+
+nextbloom.onclick = function () {
+    start--;
+    if (start == -1) {
+        start = sliderbloom.querySelectorAllll("li").lenght - 1
+    }
+    sliderbloom.style.left = -800 * start + "px"
 }
